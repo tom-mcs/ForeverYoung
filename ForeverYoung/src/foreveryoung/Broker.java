f9250a839acb69cf6f1bd5d06da0bffab5d082fe
tom <tom.mcsk@gmail.com>
2018-03-29 12:00:15 +1030

    Login Interface

--- ForeverYoung/src/foreveryoung/Broker.java
+++ ForeverYoung/src/foreveryoung/Broker.java
@@ -2,6 +2,9 @@ package foreveryoung;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,15 +18,24 @@ import java.sql.DriverManager;
  */
 public class Broker {
     
-     public Broker() throws Exception {
+     public Broker(){
      /* ------- Start DB ----------- */
         final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-        Class.forName(driver).newInstance();
-
+        try{
+            Class.forName(driver).newInstance();
+        }
+        catch(Exception E){
+            E.printStackTrace();
+        }
         final String protocol = "jdbc:derby:";
         final String dbName = "derbyDB";
-        Connection connection = DriverManager.getConnection(
-                protocol + dbName + ";create=true");
+        try {
+             Connection connection = DriverManager.getConnection(
+                     protocol + dbName + ";create=true");
+        } 
+        catch (SQLException ex) {
+             Logger.getLogger(Broker.class.getName()).log(Level.SEVERE, null, ex);
+        }
         System.out.println("=====    Started/Connected DB    =====");
      }
     
