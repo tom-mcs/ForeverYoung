914a7819fa27ecd8045449c513f093465d08637c
Ryan_Lancer <Ryan_Lancer@RC>
2018-03-29 11:48:07 +1030

    Testing

--- ForeverYoung/src/foreveryoung/Broker.java
+++ ForeverYoung/src/foreveryoung/Broker.java
@@ -1,5 +1,8 @@
 package foreveryoung;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,20 @@ package foreveryoung;
  * @author Ryan_Lancer
  */
 public class Broker {
+    
+     public static void main(String[] args) throws Exception {
+     /* ------- Start DB ----------- */
+        final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+        Class.forName(driver).newInstance();
+
+        final String protocol = "jdbc:derby:";
+        final String dbName = "derbyDB";
+        Connection connection = DriverManager.getConnection(
+                protocol + dbName + ";create=true");
+        System.out.println("=====    Started/Connected DB    =====");
+     }
+    
+    
+    
     
 }
