87dcebfa8ea1d2107237d73d2c9a43db68d4c902
Ryan <>
2018-05-30 12:42:29 +0930

    Fixed up some broker tests

--- ForeverYoung/test/foreveryoung/BrokerTest.java
+++ ForeverYoung/test/foreveryoung/BrokerTest.java
@@ -30,8 +30,8 @@ public class BrokerTest {
     
     Broker broke;
     public LocalDateTime date;
-    public int minutes;
-    public int steps;
+    public int minutes = 5;
+    public int steps = 10;
     
     @Before
     public void setUp() throws SQLException {
@@ -72,9 +72,10 @@ public class BrokerTest {
     @Test
     public void testGetUser() {
         System.out.println("getUser");
-        Practitioner samp = new Practitioner("Practitioner01", "Richard", "Scary", "apple"); 
-        User expResult = samp;
-        User result = broke.getUser(samp.getUsername());
+        Practitioner user = new Practitioner("Prac06", "Jerry", "Lion", "Scary");
+        broke.addPractitioner(user);
+        User expResult = user;
+        User result = broke.getUser(user.getUsername());
         assertEquals(expResult, result);
     }
 
@@ -104,7 +105,7 @@ public class BrokerTest {
     /**
      * Test of deleteDB method, of class Broker.
      */
-    @Test
+    //@Test
     public void testDeleteDB() {
         System.out.println("deleteDB");
         broke.deleteDB();
@@ -116,7 +117,7 @@ public class BrokerTest {
     @Test
     public void testGetDBVersionNum() {
         System.out.println("getDBVersionNum");
-        int expResult = 10;
+        int expResult = 11;
         int result = broke.getDBVersionNum();
         assertEquals(expResult, result);
 
@@ -124,11 +125,14 @@ public class BrokerTest {
 
     /**
      * Test of addPractitioner method, of class Broker.
+     * //will only work once if the parameters for the practitioner is changed. 
+     * Expected to fail if run for a second time or more, since there is no method
+     * implemented to remove a practitioner account. 
      */
     @Test
     public void testAddPractitioner() {
         System.out.println("addPractitioner");
-        Practitioner user = new Practitioner("Prac05", "apple", "Richard", "Scary"); 
+        Practitioner user = new Practitioner("Prac06", "Jerry", "Lion", "Scary"); 
         boolean result = broke.addPractitioner(user);
         boolean expResult = true;
         assertEquals(expResult, result);
@@ -182,13 +186,16 @@ public class BrokerTest {
     @Test
     public void testAddExerciseEntry() {
         System.out.println("addExerciseEntry");
+        
         AerobicExercise exercise = new AerobicExercise("Walk");
-        User sample = new User();
-        String practitioner = sample.getUsername();
-        Client client = new Client(sample, practitioner);
-        boolean expResult = false;
+        User sample = new User("Use", "pear", "Hamilton", "Pope");
+        Client client = new Client(sample, "practitioner01");
+        broke.addClient(client);
+        boolean expResult = true;
         boolean result = broke.addExercise(exercise, client);
+        broke.removeUser(client);
         assertEquals(expResult, result);
+        
     }
 
     /**
@@ -252,13 +259,15 @@ public class BrokerTest {
     @Test
     public void testAddExercise() {
         System.out.println("addExercise");
-        AerobicExercise exercise = new AerobicExercise("Running");
+        AerobicExercise exercise = new AerobicExercise("Walking");
         User user = new User("Royal", "fqac", "Elizabeth", "Mary"); 
         Client sample = new Client(user, "Practitioner01");  
-        broke.removeUser(sample);
+        broke.addClient(sample);
         boolean expResult = true;
         boolean result = broke.addExercise(exercise, sample);
+        broke.removeUser(sample);
         assertEquals(expResult, result);
+        
     }
 
     /**
@@ -269,7 +278,8 @@ public class BrokerTest {
         System.out.println("getWeightExercise");
         String exerciseName = "Walk";
         User user = new User("Royal", "fqac", "Elizabeth", "Mary");
-        Client sample = new Client(user, "Practitioner01");  
+        Client sample = new Client(user, "Practitioner01"); 
+        broke.addClient(sample);
         WeightExercise expResult = new WeightExercise(exerciseName);
         WeightExercise result = broke.getWeightExercise(exerciseName, sample);
         assertEquals(expResult, result);
@@ -307,12 +317,16 @@ public class BrokerTest {
     @Test
     public void testAddAerobicExerciseEntry() {
         System.out.println("addAerobicExerciseEntry");
+        LocalDateTime today = LocalDateTime.now();
+        int minute = 5;
         AerobicExercise exercise = new AerobicExercise("Rowing");
-        AerobicExerciseEntry exerciseEntry = new AerobicExerciseEntry(date, minutes);
+        AerobicExerciseEntry exerciseEntry = new AerobicExerciseEntry(today, minute);
         User user = new User("Royal", "fqac", "Elizabeth", "Mary");
-        Client sample = new Client(user, "Practitioner01");  
+        Client sample = new Client(user, "Practitioner01");
+        broke.addClient(sample);
         boolean expResult = true;
         boolean result = broke.addAerobicExerciseEntry(exercise, exerciseEntry, sample);
+        broke.removeUser(sample);
         assertEquals(expResult, result);
     }
 
@@ -322,15 +336,19 @@ public class BrokerTest {
     @Test
     public void testAddWeightExerciseEntry() {
         System.out.println("addWeightExerciseEntry");
-        WeightExercise exercise = new WeightExercise("weights");
-        LocalDateTime today = null;
+        User user = new User("Royal", "fqac", "Elizabeth", "Mary");
+        Client sample = new Client(user, "practitioner01");
+        broke.removeUser(sample);
+        WeightExercise exercise = new WeightExercise("Chest Press");
+        LocalDateTime today = LocalDateTime.now();
         Set setA = new Set(1, 5); 
         Set setB = new Set(3, 9);
         WeightExerciseEntry exerciseEntry = new WeightExerciseEntry(today, setA, setB);
-        User user = new User("Royal", "fqac", "Elizabeth", "Mary");
-        Client sample = new Client(user, "Ryan");  
+        
+        broke.addClient(sample);
         boolean expResult = true;
         boolean result = broke.addWeightExerciseEntry(exercise, exerciseEntry, sample);
+    
         assertEquals(expResult, result);
     }
 
